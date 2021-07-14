e6ec5395cd63a200b61bbb12f713b53d69eae0c5
swati-0308 <60496689+swati-0308@users.noreply.github.com>
2021-07-14 17:34:34 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,49 @@
+#Importing the libraries required
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Loading the iris dataset
+from sklearn import datasets
+iris = datasets.load_iris()
+iris_df = pd.DataFrame(iris.data, columns = iris.feature_names)
+iris_df 
+
+#Finding the optimum number of clusters for K-means classification
+x = iris_df.iloc[:, [0, 1, 2, 3]].values
+
+from sklearn.cluster import KMeans
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', 
+                    max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+    
+# Plotting the results onto a line graph, to observe 'The Elbow'
+plt.plot(range(1, 11), wcss)
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS') # Within cluster sum of squares
+plt.show()
+
+#Applying kmeans to the dataset
+kmeans = KMeans(n_clusters = 3, init = 'k-means++',
+                max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+print(y_kmeans)
+
+#Visualising the clusters 
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], 
+            s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], 
+            s = 100, c = 'blue', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1],
+            s = 100, c = 'green', label = 'Iris-virginica')
+
+#Plotting the centroids of the clusters
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], 
+            s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
